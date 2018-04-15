508cdd219d7629cde1193825b6d0960294603dc9
RafalSwat <37617768+RafalSwat@users.noreply.github.com>
2018-04-15 16:07:35 +0200

    Create sol_4_1.h

---
+++ cpp_list4/sol_4_1.h
@@ -0,0 +1,6 @@
+#include <iostream>
+#include <vector>
+
+void show(std::vector<double>& result);
+double &operator*(std::vector<double>& v, std::vector<double>& w);
+std::vector<double> fill_vector(std::vector<double>& vector);
